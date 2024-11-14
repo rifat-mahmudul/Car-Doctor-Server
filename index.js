f5f4d9c24b8c1449cf3b2290dda56d5a295ee44d
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-14 11:38:01 +0600

    get request done

--- index.js
+++ index.js
@@ -37,6 +37,12 @@ async function run() {
             res.send(result);
         })
 
+        //get all service
+        app.get('/services', async(req, res) => {
+            const result = await serviceCollection.find().toArray();
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
