8acd4da7543728493c04df475d246c19ae557793
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-21 22:37:00 +0600

    token done

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin : ['http://localhost:5173'],
+    credentials : true
+}));
 app.use(express.json());
 
 
@@ -29,6 +33,32 @@ async function run() {
         
         const serviceCollection = client.db('ServiceDB').collection('services');
         const ordersCollection = client.db('ServiceDB').collection('orders');
+
+        //auth related api
+        app.post('/jwt', async(req, res) => {
+            const user = req.body;
+            console.log('user for token', user)
+            //create a token
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN , {expiresIn : "1h"});
+
+            //set cookie
+            res
+            .cookie('token', token, {
+                httpOnly : true,
+                secure : true,
+                sameSite : 'none'
+            })
+            .send({success : true})
+        })
+
+        app.post('/logout', async(req, res) => {
+            const user = req.body;
+            console.log('log in out', user)
+            res
+            .clearCookie('token', 
+            {maxAge : 0})
+            .send({success: true})
+        })
 
         //create a service Data
         app.post('/services', async(req, res) => {
@@ -55,6 +85,19 @@ async function run() {
         app.post('/orders', async(req, res) => {
             const order = req.body;
             const result = await ordersCollection.insertOne(order);
+            res.send(result);
+        })
+
+        //get single orders Data
+        app.get('/orders', async(req, res) => {
+            const result = await ordersCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.delete('/orders/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await ordersCollection.deleteOne(query)
             res.send(result);
         })
 
